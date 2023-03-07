ce04acccc42684e371b6ddccdf63ccfadc92f774
Fabián Payan <f.payan@uniandes.edu.co>
2023-03-07 14:30:11 -0500

    Update persona.py

--- src/persona.py
+++ src/persona.py
@@ -19,9 +19,9 @@ class Persona:
    def dar_nombre(self):
        return(self.__nombre)
 
-   def calcular_anio_nacimiento(self, ya_cumplio_anios):   
+   def calcular_anio_nacimiento(self, ya_cumplio_anios):
        anio_actual = datetime.datetime.now().year
        if ya_cumplio_anios:
-           return (anio_actual - self.__edad) 
+           return (anio_actual - self.__edad)
        else:
            return (anio_actual - self.__edad + 1)
