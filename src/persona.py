564c6dbbddd12bb607d150bb6f24a7396ccfd58f
Fabián Payan <f.payan@uniandes.edu.co>
2023-03-07 11:03:04 -0500

    Testing merge #5

--- src/persona.py
+++ src/persona.py
@@ -19,7 +19,7 @@ class Persona:
    def dar_nombre(self):
        return(self.__nombre)
 
-   def calcular_anio_nacimiento(self, ya_cumplio_anios):
+   def calcular_anio_nacimiento(self, ya_cumplio_anios):   
        anio_actual = datetime.datetime.now().year
        if ya_cumplio_anios:
            return (anio_actual - self.__edad)
